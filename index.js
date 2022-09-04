9abe3a6b8603949b4a98a6652254d1fce4a5f621
Mercy <mercycheruiyot2013@gmail.com>
2022-09-04 07:32:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+ //const titleCased = tutorials.map((element) => {
+  // return  element.toLowerCase().replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
+
+ //});
+ //console.log(titleCased);
+
+ function titleCased(){
+  return tutorials.map(element=> {
+     return  element.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
+  } )};
+ console.log(titleCased(tutorials));
+
